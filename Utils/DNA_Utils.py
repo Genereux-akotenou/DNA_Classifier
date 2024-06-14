ace4b92b02a0d3548044f79468af6f309069abcc
Généreux AKOTENOU <genereux.akotenou@gmail.com>
2024-06-14 14:48:10 +0100

    init: classsification kiwi and orange

--- Utils/DNA_Utils.py
+++ Utils/DNA_Utils.py
@@ -1,4 +1,7 @@
 import numpy as np
+import itertools
+import pandas as pd
+from sklearn.feature_extraction import DictVectorizer
 
 class DNA:
     @staticmethod
@@ -28,3 +31,64 @@ class DNA:
                     one_hot_encoded[i, j, nucleotide_to_int[nucleotide]] = 1
 
         return one_hot_encoded
+
+    @staticmethod
+    def kmer_count(sequence, k=3, step=1):
+        """
+        Utils: to count kmer occurence in DNA sequence and compute frequence
+        """
+        kmers = [''.join(p) for p in itertools.product('ACGT', repeat=k)]
+        kmers_count = {kmer: 0 for kmer in kmers}
+        s = 0
+        for i in range(0, len(sequence) - k + 1, step):
+            kmer = sequence[i:i + k]
+            s += 1
+            #kmers_count[kmer] += 1
+            if kmer in kmers_count:
+                kmers_count[kmer] += 1
+            #else:
+            #    kmers_count[kmer] = 1
+        for key, value in kmers_count.items():
+            kmers_count[key] = value / s
+    
+        return kmers_count
+
+    @staticmethod
+    def read_fasta_file(file_path, family):
+        """
+        Utils: Convert fasta file to dataframe
+        """
+        sequences = []
+        with open(file_path, 'r') as file:
+            current_id = None
+            current_sequence = ''
+            for line in file:
+                if line.startswith('>'):
+                    if current_id:
+                        sequences.append({'id': current_id, 'sequence':current_sequence, 'length':len(current_sequence), 'class': family})
+                    current_id = line.strip().split('|')[0][1:].strip()
+                    current_sequence = ''
+                else:
+                    current_sequence += line.strip()
+            if current_id:
+                sequences.append({'id': current_id, 'sequence':current_sequence, 'length':len(current_sequence), 'class': family})
+        
+        df = pd.DataFrame(sequences)
+        return df
+
+    @staticmethod
+    def build_kmer_representation(df, k=3):
+        """
+        Utils: For given k-mer generate dataset and return vectorised version
+        """
+        sequences   = df['sequence']
+        kmers_count = []
+        for i in range(len(sequences)):
+            kmers_count.append(DNA.kmer_count(sequences[i], k=k, step=1))
+            
+        v = DictVectorizer(sparse=False)
+        feature_values = v.fit_transform(kmers_count)
+        feature_names = v.get_feature_names_out()
+        X = pd.DataFrame(feature_values, columns=feature_names)
+        y = df['class']
+        return X, y
